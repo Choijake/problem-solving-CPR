d4846987f6c0a954434631569e2d1579e4bf6d26
Choijake <shameless8@naver.com>
2025-06-10 20:34:20 +0900

    [PG] 전화 번호 목록 / Level2

---
+++ 최재익/programmers/PG_20250610_K번째수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int[] array, int[][] commands) {
+        List<Integer> answer = new ArrayList<>();
+
+        for(int[] command : commands){
+            int start = command[0];
+            int end = command[1];
+            int target = command[2];
+
+            int[] part = Arrays.copyOfRange(array, start-1, end);
+            Arrays.sort(part);
+            answer.add(part[target-1]);
+        }
+
+        return answer;
+    }
+}
