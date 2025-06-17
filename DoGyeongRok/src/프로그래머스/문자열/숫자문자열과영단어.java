a5e4105915af8829c74ae84057735d30dad32fe9
도경록 <dkr0210@naver.com>
2025-06-17 17:38:47 +0900

    feat: 프로그래머스 '가장 가까운 글자'

--- DoGyeongRok/src/프로그래머스/문자열/숫자문자열과영단어.java
+++ DoGyeongRok/src/프로그래머스/문자열/숫자문자열과영단어.java
@@ -1,5 +1,8 @@
 package 프로그래머스.문자열;
 
+// 2025-06-17
+// https://school.programmers.co.kr/learn/courses/30/lessons/81301
+
 public class 숫자문자열과영단어 {
     class Solution {
         public int solution(String s) {
