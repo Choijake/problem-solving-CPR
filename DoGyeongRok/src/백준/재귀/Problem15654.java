615f99bdd73813f43b3bd106edc619912ff70d32
도경록 <dkr0210@naver.com>
2025-06-27 16:57:39 +0900

    feat: 백준 'N과 M(5)(15654 번)'

--- DoGyeongRok/src/백준/재귀/Problem15654.java
+++ DoGyeongRok/src/백준/재귀/Problem15654.java
@@ -1,4 +1,50 @@
 package 백준.재귀;
 
+// 2025-06-27
+// N과 M(5)
+// https://www.acmicpc.net/problem/15654
+
+import java.io.*;
+import java.util.*;
+
 public class Problem15654 {
+    private static int N, M;
+    private static int[] arr, result;
+    private static boolean[] isVisited;
+    private static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        isVisited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        result = new int[M];
+        rf(0);
+        System.out.println(sb);
+    }
+
+    public static void rf(int depth) {
+        if(depth == M) {
+            for(int num: result) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i = 0; i < N; i++) {
+            if(!isVisited[i]) {
+                isVisited[i] = true;
+                result[depth] = arr[i];
+                rf(depth + 1);
+                isVisited[i] = false;
+            }
+        }
+    }
 }
