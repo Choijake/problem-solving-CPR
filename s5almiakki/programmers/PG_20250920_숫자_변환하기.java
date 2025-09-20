6344cad43291c62311f8b86758c4fdc4ded1044b
5almiakki <jpui9635@gmail.com>
2025-09-20 23:33:48 +0900

    feat: BFS로 숫자 변환하기 풀이
    y에서 시작해서 x로 가는 방식으로의 풀이 추가 (더 빠름)

--- s5almiakki/programmers/PG_20250920_숫자_변환하기.java
+++ s5almiakki/programmers/PG_20250920_숫자_변환하기.java
@@ -34,4 +34,44 @@ public class PG_20250920_숫자_변환하기 {
 
     }
 
+    class Solution2 {
+
+        public int solution(int x, int y, int n) {
+            boolean[] visited = new boolean[y + 1];
+            Queue<int[]> queue = new ArrayDeque<>();
+            queue.add(new int[] { y, 0 });
+            visited[y] = true;
+            int answer = Integer.MAX_VALUE;
+            do {
+                int[] currentElement = queue.remove();
+                if (currentElement[0] == x) {
+                    answer = Math.min(answer, currentElement[1]);
+                    continue;
+                }
+                int newCount = currentElement[1] + 1;
+                int newValue = currentElement[0] - n;
+                if (newValue >= 0 && !visited[newValue]) {
+                    queue.add(new int[] { newValue, newCount });
+                    visited[newValue] = true;
+                }
+                if (currentElement[0] % 2 == 0) {
+                    newValue = currentElement[0] >> 1;
+                    if (!visited[newValue]) {
+                        queue.add(new int[] { newValue, newCount });
+                        visited[newValue] = true;
+                    }
+                }
+                if (currentElement[0] % 3 == 0) {
+                    newValue = currentElement[0] / 3;
+                    if (!visited[newValue]) {
+                        queue.add(new int[] { newValue, newCount });
+                        visited[newValue] = true;
+                    }
+                }
+            } while (!queue.isEmpty());
+            return answer == Integer.MAX_VALUE ? -1 : answer;
+        }
+
+    }
+
 }
